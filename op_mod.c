9d431fa78d6dc065befe2b2529f36e78d5186ec0
FourtyThree43 <shaqmwa@outlook.com>
2023-03-27 02:27:31 +0300

    fix

--- op_mod.c
+++ op_mod.c
@@ -12,7 +12,7 @@ void op_mod(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
@@ -21,7 +21,7 @@ void op_mod(stack_t **stack, unsigned int line_number)
 
 	if (b == 0)
 	{
-		fprintf(stderr, "L%u: division by zero\n", line_number);
+		fprintf(stderr, "L%d: division by zero\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
